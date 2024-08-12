1ba41185e41009c13bcd604685b3a5d5bacb0e22
Petruttt <alin.petru46@gmail.com>
2024-08-12 17:49:32 +0300

    modele pagini si rute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 
 export default function App() {
   return (
-    <h1 className='text-xl'>
-      App
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path= "/About" element={<About/>}/>
+      <Route path= "/dashboard" element={<Dashboard/>}/>
+      <Route path= "/home" element={<Home/>}/>
+      <Route path= "/signin" element={<SignIn/>}/>
+      <Route path= "/signup" element={<SignUp/>}/>
+      
+    </Routes>
+    </BrowserRouter>
   )
 }
