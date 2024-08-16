7023b39a3eb6babdef937db4f993e676c683fd40
Petruttt <alin.petru46@gmail.com>
2024-08-16 17:14:27 +0300

    Conectare la baza de date

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('MongoDB is connected');
+})
 const app=express();
 app.listen(3000,()=>{
     console.log('Server is running on port 3000 sef');
