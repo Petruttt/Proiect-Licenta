b9057ef8daeb844c881e46a1eb84f74e895c92dc
Petruttt <alin.petru46@gmail.com>
2024-08-19 15:46:11 +0300

    signup funct

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -10,13 +10,15 @@ export default function SignUp() {
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value.trim()})
   };
-  const handleSubmit = async (e) => {
+  //console.log(formData);
+  
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    if(!formData.username || !formData.email || !formData.password) {
-    return setErrorMessage('Please fill in all fields');
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill in all the fields');
     }
-
-    try {
+    
+    try{
       setLoading(true);
       setErrorMessage(null);
       const response = await fetch('/api/auth/signup',{
@@ -27,28 +29,27 @@ export default function SignUp() {
         body: JSON.stringify(formData)
       });
       const data = await response.json();
-      if(data.success === false) {
-        return setErrorMessage(data.message);
-      }
-      setLoading(false);
-      if(response.ok)
-        {
-          navigate('/sign-in');
-        }
       console.log(data);
-    } catch (error) {
-      console.log(error);
+      if(data.success == false){
+        setErrorMessage(data.message);
+      }
+      if(response.ok){
+        navigate('/sign-in');
+      }
+    }catch(error){
       setErrorMessage(error.message);
-      setLoading(false);
+      
     }
+    setLoading(false);
+    
   }
   return (
     <div className ='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+      <div className='flex flex-col max-w-3xl gap-5 p-3 mx-auto md:flex-row md:items-center'>
         {/*left side*/}
         <div className='flex flex-col items-center'>
           <Link to="/" className='font-bold dark:text-white'>
-            <span className='px-2 py-1 bg-gradient-to-r from-purple-700 to-blue-900 rounded-lg text-white text-4xl mr-7'>WebVoyage</span>
+            <span className='px-2 py-1 text-4xl text-white rounded-lg bg-gradient-to-r from-purple-700 to-blue-900 mr-7'>WebVoyage</span>
           </Link>
           <p className='mt-2 text-center mr-7'>
             Sign up to get access to all the features of our site
@@ -91,7 +92,7 @@ export default function SignUp() {
             }
           </Button>
         </form>
-        <div className='flex gap-2 text-sm mt-5'>
+        <div className='flex gap-2 mt-5 text-sm'>
           <span>
             Already have an account?
           </span>
