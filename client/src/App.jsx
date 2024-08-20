08148c4fa5e8d26b1429e6ea207398f069361444
Petruttt <alin.petru46@gmail.com>
2024-08-20 22:21:58 +0300

    dashboard privat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import Sites from './pages/Sites'
 import FooterComponent from './components/Footer'
-
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -16,10 +16,12 @@ export default function App() {
     <Header/> 
     <Routes>
       <Route path= "/About" element={<About/>}/>
-      <Route path= "/dashboard" element={<Dashboard/>}/>
       <Route path= "/home" element={<Home/>}/>
       <Route path= "/sign-in" element={<SignIn/>}/>
       <Route path= "/sign-up" element={<SignUp/>}/>
+      <Route element = {<PrivateRoute/>}>
+      <Route path= "/dashboard" element={<Dashboard/>}/>
+      </Route>
       <Route path="/sites" element = {<Sites/>}/>
       
     </Routes>
