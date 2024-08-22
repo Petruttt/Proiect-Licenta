70ef079170fc3641e5ac069e1e0b28ed134b4dca
Petruttt <alin.petru46@gmail.com>
2024-08-22 21:10:48 +0300

    admin role

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema({
         type:String,
         default:"https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg",
     },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+    },
 },{timestamp:true});
 
 const User = mongoose.model('User',userSchema);
