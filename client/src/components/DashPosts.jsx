02bcd80cb5b68e1c26cda5a56e7d7ca39f4612a4
Petruttt <alin.petru46@gmail.com>
2024-08-25 23:56:52 +0300

    showmore posts button

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 export default function DashPosts() {
   const {currentUser} = useSelector(state => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
@@ -13,7 +14,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
-          
+          if(data.post.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -23,12 +26,24 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+  
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
-    <div className='table-auto overflow-x-scroll md:mx-auto
-     p-3 scrollbar scrollbar-track-slate-100
-      scrollbar-thumb-slate-300 
-      dark:scrollbar-track-slate-700 
-      dark:scrollbar-thumb-slate-500'>
+    <div className='p-3 overflow-x-scroll table-auto md:mx-auto scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {userPosts.length > 0 ? (
         <>
         <Table hoverable className='shadow-md'>
@@ -64,7 +79,7 @@ export default function DashPosts() {
                 <img 
                 src={post.image} 
                 alt={post.title} 
-                className='w-20 h-10 object-cover bg-gray-500'/>
+                className='object-cover w-20 h-10 bg-gray-500'/>
                 </Link>
               </Table.Cell>
               <Table.Cell>
@@ -76,7 +91,7 @@ export default function DashPosts() {
                 {post.category}
               </Table.Cell>
               <Table.Cell>
-                <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
+                <span className='font-medium text-red-500 cursor-pointer hover:underline'>Delete</span>
               </Table.Cell>
               <Table.Cell>
                 <Link className='text-blue-500 hover:underline' to={`/update-post/${post._id}`}>
@@ -88,6 +103,14 @@ export default function DashPosts() {
           ))}
 
         </Table>
+        {showMore && (
+          <div className='flex justify-center'>
+            <button onClick={handleShowMore}
+             className='px-3 py-1 mt-3 text-white bg-gray-500 rounded-md '>
+              Show More
+            </button>
+          </div>
+        )}
         </>
       ):
       <p>No Posts Yet</p>
